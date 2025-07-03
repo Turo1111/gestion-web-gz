0ff139cb87763ed3e2dc969d14c61245597b7478
turo1111 <Matiaszurita1998@gmail.com>
2025-07-03 18:05:57 -0300

    fix token

--- src/utils/client.tsx
+++ src/utils/client.tsx
@@ -3,6 +3,20 @@ import axios from "axios";
 const apiClient = axios.create({
   // URL para variable de entorno
   baseURL: process.env.NEXT_PUBLIC_DB_HOST
-}); 
+});
+
+apiClient.interceptors.request.use(
+  async (config) => {
+    const token = window.localStorage.getItem('user')
+    if (token) {
+      const tokenParsed = JSON.parse(token)
+      config.headers.Authorization = `Bearer ${tokenParsed.token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default apiClient;
